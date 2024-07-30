970ebb6bbfaa14686e026e6ed75afde45784717b
Hermino Gurgel <hermino.gurgel@gmail.com>
2024-07-30 11:06:29 -0300

    refactor: changed the ForgotPassword.vue to use vuetify components and classes

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -1,9 +1,5 @@
 <script setup>
 import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
 import { Head, useForm } from '@inertiajs/vue3';
 
 defineProps({
@@ -25,37 +21,31 @@ const submit = () => {
     <GuestLayout>
         <Head title="Forgot Password" />
 
-        <div class="mb-4 text-sm text-gray-600">
+        <div class="mb-4 text-body-2">
             Forgot your password? No problem. Just let us know your email address and we will email you a password reset
             link that will allow you to choose a new one.
         </div>
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+        <div v-if="status" class="mb-4">
             {{ status }}
         </div>
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
+        <v-form @submit.prevent="submit">
+            <v-text-field
+                v-model="form.email"
+                label="Email"
+                :error-messages="form.errors.email"
+            />
 
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
+            <div class="d-flex justify-end mt-4">
+                <v-btn
+                    type="submit"
+                    color="black"
+                    :disabled="form.processing"
+                    class="ms-4 text-capitalize"
+                    text="email password reset link"
                 />
-
-                <InputError class="mt-2" :message="form.errors.email" />
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Email Password Reset Link
-                </PrimaryButton>
-            </div>
-        </form>
+        </v-form>
     </GuestLayout>
 </template>
