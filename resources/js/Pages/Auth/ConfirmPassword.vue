df43b18b16a620aefd4cb366b99e176bc58349ae
Hermino Gurgel <hermino.gurgel@gmail.com>
2024-07-30 19:01:33 -0300

    refactor: changed ConfirmPassword.vue.vue to use vuetify components and classes

--- resources/js/Pages/Auth/ConfirmPassword.vue
+++ resources/js/Pages/Auth/ConfirmPassword.vue
@@ -1,9 +1,5 @@
 <script setup>
 import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
 import { Head, useForm } from '@inertiajs/vue3';
 
 const form = useForm({
@@ -21,30 +17,28 @@ const submit = () => {
     <GuestLayout>
         <Head title="Confirm Password" />
 
-        <div class="mb-4 text-sm text-gray-600">
+        <div class="mb-4 text-body-2">
             This is a secure area of the application. Please confirm your password before continuing.
         </div>
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="password" value="Password" />
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="current-password"
-                    autofocus
-                />
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
+        <v-form @submit.prevent="submit">
 
-            <div class="flex justify-end mt-4">
-                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Confirm
-                </PrimaryButton>
+            <v-text-field
+                v-model="form.password"
+                label="Password"
+                :error-messages="form.errors.password"
+                type="password"
+            />
+
+            <div class="d-flex justify-end mt-4">
+                <v-btn
+                    type="submit"
+                    color="black"
+                    :disabled="form.processing"
+                    class="ms-4 text-capitalize"
+                    text="confirm"
+                />
             </div>
-        </form>
+        </v-form>
     </GuestLayout>
 </template>
