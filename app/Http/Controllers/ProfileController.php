30e6904d6005bb897b82a2ce26c532727e4132f4
Hermini Gurgel <herminio.gurgel@gmail.com>
2024-11-15 12:36:20 -0300

    refactor: reorganize project structure and directories
    - Move files to more appropriate directories for better organization
    - Remove obsolete or unused files and folders

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -18,7 +18,7 @@ class ProfileController extends Controller
      */
     public function edit(Request $request): Response
     {
-        return Inertia::render('Profile/EditPage', [
+        return Inertia::render('EditPage', [
             'mustVerifyEmail' => $request->user() instanceof MustVerifyEmail,
             'status' => session('status'),
         ]);
